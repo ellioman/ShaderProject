792a1d72320e929765e7c988815b4b28c1526477
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2015-11-23 21:42:37 +0100

    Created a new scene that uses the stencil buffer

---
+++ Assets/Projects/Stencils/Scripts/MaskFactory.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class MaskFactory : MonoBehaviour
+{
+	#region Variables
+	
+	// Unity Editor Variables
+	[SerializeField] protected GameObject[] maskPrefabs;
+	[SerializeField] protected float spawnDelay;
+
+	// Protected Instance Variables
+	protected int maskIndex = 0;
+	protected float lastSpawnTime = 0f;
+
+	#endregion
+
+	// Constructor
+	protected void Awake()
+	{
+		Assert.IsTrue(maskPrefabs.Length > 0);
+	}
+
+	// Use this for initialization
+	protected void Start()
+	{
+		//SpawnObject();
+		lastSpawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	protected void Update()
+	{
+		if (Time.time - lastSpawnTime > spawnDelay)
+		{
+			SpawnObject();
+		}
+	}
+
+	protected void SpawnObject()
+	{
+		lastSpawnTime = Time.time;
+		GameObject mask = Instantiate(maskPrefabs[maskIndex]);
+		mask.transform.position = transform.position;
+		mask.transform.parent = transform;
+		maskIndex = (maskIndex + 1) % maskPrefabs.Length;
+	}
+}
---
+++ Assets/Projects/Stencils/Scripts/MoveToAndDie.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToAndDie : MonoBehaviour
+{
+	#region Variables
+
+	// Unity Editor Variables
+	[SerializeField] protected float speed;
+	[SerializeField] protected Vector3 targetPos;
+
+	// Protected Instance Variables
+	protected Vector3 direction = Vector3.zero;
+
+	#endregion
+	
+	// Update is called once per frame
+	protected void Update ()
+	{
+		direction = (targetPos - transform.position).normalized;
+		transform.Translate(direction * Time.deltaTime * speed);
+
+		// Reset once we've reached our target position...
+		if ((targetPos - transform.position).magnitude < 1f)
+		{
+			Destroy(gameObject);
+			enabled = false;
+		}
+	}
+
+
+}
