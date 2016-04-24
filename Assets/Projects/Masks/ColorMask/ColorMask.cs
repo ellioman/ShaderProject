040a1be8e8a367d14c1648ea30dde52f4bffe66f
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2016-04-24 02:36:11 +0200

    New Projects: ColorMask and ColorAlphaMask

---
+++ Assets/Projects/Masks/ColorMask/ColorMask.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorMask : MonoBehaviour
+{
+	#region Variables
+
+	// Unity Editor Variables
+	[SerializeField] protected Shader shader;
+
+	// Protected Instance Variables
+	protected Material mat;
+
+	#endregion
+
+	#region MonoBehaviour
+
+	// Called on the frame when a script is enabled just before 
+	// any of the Update methods is called the first time.
+	protected void Start()
+	{
+		Renderer rend = GetComponent<Renderer>();
+		if (rend && rend.material)
+		{
+			mat = rend.material;
+		}
+		else
+		{
+			enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	protected void Update ()
+	{
+		mat.SetFloat("_AlphaAmount", Mathf.Sin(Time.time));
+	}
+
+	// Called when the behaviour becomes disabled, inactive or when 
+	// the object is destroyed and can be used for any cleanup code
+	protected void OnDisable()
+	{
+		if (mat)
+		{
+			DestroyImmediate(mat);
+		}
+	}
+
+	#endregion
+}
