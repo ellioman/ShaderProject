558595c56425b320731d6699a38b4f6f5b41221a
ellioman <ellioman@gmail.com>
2017-07-13 23:36:05 +0200

    Basic shader added with properties controlling blending, blending operation, culling, ztesting, zwriting, depth offset factor and units,

---
+++ Assets/Projects/_BasicShader/BasicShader.shader
@@ -0,0 +1,124 @@
+﻿Shader "Ellioman/BasicShader"
+{
+	Properties
+	{
+		_MainTex ("Main Texture", 2D) = "white" {}
+		_Color ("Color", Color) = (1,1,1,1)
+
+		[Enum(Zero, 0, One,1, DstColor,2, SrcColor,3, OneMinusDstColor,4, SrcAlpha, 5, OneMinusSrcColor, 6, DstAlpha, 7, OneMinusDstAlpha, 8, SrcAlphaSaturate, 9, OneMinusSrcAlpha,10)] _BlendModeSourceColor ("Source Color BlendMode", Int) = 1
+		[Enum(Zero, 0, One,1, DstColor,2, SrcColor,3, OneMinusDstColor,4, SrcAlpha, 5, OneMinusSrcColor, 6, DstAlpha, 7, OneMinusDstAlpha, 8, SrcAlphaSaturate, 9, OneMinusSrcAlpha,10)] _BlendModeDestinationColor ("Destination Color BlendMode", Int) = 0
+		[Enum(Zero, 0, One,1, DstColor,2, SrcColor,3, OneMinusDstColor,4, SrcAlpha, 5, OneMinusSrcColor, 6, DstAlpha, 7, OneMinusDstAlpha, 8, SrcAlphaSaturate, 9, OneMinusSrcAlpha,10)] _BlendModeSourceAlpha ("Source Alpha BlendMode", Int) = 1
+		[Enum(Zero, 0, One,1, DstColor,2, SrcColor,3, OneMinusDstColor,4, SrcAlpha, 5, OneMinusSrcColor, 6, DstAlpha, 7, OneMinusDstAlpha, 8, SrcAlphaSaturate, 9, OneMinusSrcAlpha,10)] _BlendModeDestinationAlpha ("Destination Alpha BlendMode", Int) = 0
+		[Enum(Add, 0, Subtract, 1, ReverseSubtract, 2, Min, 3, Max, 4, LogicalClear, 5, LogicalSet, 6, LogicalCopy, 7, LogicalCopyInverted, 8, LogicalNoop, 9, LogicalInvert, 10, LogicalAnd, 11, LogicalNand, 12, LogicalOr, 13, LogicalNor, 14, LogicalXor, 15, LogicalEquivalence, 16, LogicalAndReverse, 17, LogicalAndInverted, 18, LogicalOrReverse, 19, LogicalOrInverted, 20, Multiply, 21, Screen, 22, Overlay, 23, Darken, 24, Lighten, 25, ColorDodge, 26, ColorBurn, 27, HardLight, 28, SoftLight, 29, Difference, 30, Exclusion, 31, HSLHue, 32, HSLSaturation, 33, HSLColor, 34, HSLLuminosity, 35, )] _BlendOpColor ("Blend Op Color", Int) = 0
+		[Enum(Add, 0, Subtract, 1, ReverseSubtract, 2, Min, 3, Max, 4, LogicalClear, 5, LogicalSet, 6, LogicalCopy, 7, LogicalCopyInverted, 8, LogicalNoop, 9, LogicalInvert, 10, LogicalAnd, 11, LogicalNand, 12, LogicalOr, 13, LogicalNor, 14, LogicalXor, 15, LogicalEquivalence, 16, LogicalAndReverse, 17, LogicalAndInverted, 18, LogicalOrReverse, 19, LogicalOrInverted, 20, Multiply, 21, Screen, 22, Overlay, 23, Darken, 24, Lighten, 25, ColorDodge, 26, ColorBurn, 27, HardLight, 28, SoftLight, 29, Difference, 30, Exclusion, 31, HSLHue, 32, HSLSaturation, 33, HSLColor, 34, HSLLuminosity, 35, )] _BlendOpAlpha ("Blend Op Alpha", Int) = 0
+
+		[Enum(Off, 0,Front,1,Back,2)] _CullMode ("CullMode", Int) = 2
+		[Enum(Less, 0, Greater, 1, LEqual, 2, GEqual, 3, Equal, 4, NotEqual, 5, Always, 6)] _ZTestMode ("Z Test Mode", Int) = 3
+		[Enum(On, 0, Off,1)] _ZWriteMode ("Z Write Mode", Int) = 0
+		_DepthOffsetFactor ("Depth Offset Factor", Float) = 0
+		_DepthOffsetUnits ("Depth Offset Units", Float) = 0
+	}
+	SubShader
+	{
+		Tags 
+		{
+			"Queue" = "Transparent"
+			"RenderType" = "Transparent"
+			"PreviewType" = "Plane"
+			// "DisableBatching" = "True" / "False" / "LODFading"
+			//"ForceNoShadowCasting" = "True"
+			//"IgnoreProjector" = "True"
+			//"CanUseSpriteAtlas" = "False"
+		}
+
+		// Shader Level of Detail (LOD) works by only using shaders or 
+		// subshaders that have their LOD value less than a given number.
+		LOD 100
+
+		// Cull
+		// Control which sides of polygons should be culled (not drawn)
+		//   Back:  Don’t render polygons facing away from the viewer (default).
+		//   Front: Don’t render polygons facing towards the viewer. Used for turning objects inside-out.
+		//   Off:   Disables culling - all faces are drawn. Used for special effects.
+		Cull [_CullMode]
+
+		// ZTest
+		// How should depth testing be performed. 
+		// Default is LEqual (draw objects in from or at the distance as existing objects; hide objects behind them).
+		// Options: Less | Greater | LEqual | GEqual | Equal | NotEqual | Always
+		ZTest [_ZTestMode]
+
+		// ZWrite
+		// Controls whether pixels from this object are written to the depth buffer (default is On). 
+		// If you’re drawng solid objects, leave this on. 
+		// If you’re drawing semitransparent effects, switch to ZWrite Off. 
+		// Options: On | Off
+		ZWrite [_ZWriteMode]
+
+		// Offset
+		// Allows you specify a depth offset with two parameters. factor and units. 
+		// Factor scales the maximum Z slope, with respect to X or Y of the polygon, 
+		// and units scale the minimum resolvable depth buffer value. This allows you 
+		// to force one polygon to be drawn on top of another although they are actually 
+		// in the same position. For example Offset 0, –1 pulls the polygon closer to the
+		// camera ignoring the polygon’s slope, whereas Offset –1, –1 will pull the polygon even closer
+		// when looking at a grazing angle.
+		Offset [_DepthOffsetFactor], [_DepthOffsetUnits]
+
+		// Blend
+		// Configure and enable blending. 
+		// The generated color is multiplied by the SrcFactor. The color already on screen is multiplied by DstFactor.
+		// The two are then added/Subtracted/etc together, which is determined by BlendOp.
+		// Note: Unity checks and disables blend in case of non blending operation set 
+		// (source == One, dest == Zero, sourceAlpha == One, destAlpha == Zero)
+		Blend [_BlendModeSourceColor] [_BlendModeDestinationColor], [_BlendModeSourceAlpha] [_BlendModeDestinationAlpha]
+
+		// Blending Operation
+		BlendOp [_BlendOpColor], [_BlendOpAlpha]
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			uniform sampler2D _MainTex;
+			uniform float4 _MainTex_ST;
+			uniform fixed4 _Color;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o, O.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv); // sample the texture
+				UNITY_APPLY_FOG(i.fogCoord, col);   // apply fog
+				return col * _Color;                // return the texture color * the color property
+			}
+			ENDCG
+		}
+	}
+
+	// Use a Custom C# Editor script for this shader
+	CustomEditor "BasicShaderMaterialEditor"
+}
---
+++ Assets/Projects/_BasicShader/Editor/BasicShaderMaterialEditor.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class BasicShaderMaterialEditor : MaterialEditor
+{
+	#region Variables
+
+	private class ShaderProperties
+	{
+		// Shader Properties Names
+		public const string MAIN_TEXTURE = "_MainTex";
+		public const string COLOR = "_Color";
+		public const string SOURCE_COLOR_BLEND_MODE = "_BlendModeSourceColor";
+		public const string DESTINATION_COLOR_BLEND_MODE = "_BlendModeDestinationColor";
+		public const string SOURCE_ALPHA_BLEND_MODE = "_BlendModeSourceAlpha";
+		public const string DESTINATION_ALPHA_BLEND_MODE = "_BlendModeDestinationAlpha";
+		public const string CULL_MODE = "_CullMode";
+		public const string Z_WRITE_MODE = "_ZWriteMode";
+		public const string Z_TEST_MODE = "_ZTestMode";
+		public const string DEPTH_OFFSET_FACTOR = "_DepthOffsetFactor";
+		public const string DEPTH_OFFSET_UNITS = "_DepthOffsetUnits";
+		public const string BLEND_OP_COLOR = "_BlendOpColor";
+		public const string BLEND_OP_ALPHA = "_BlendOpAlpha";
+
+		// Shader Property IDs
+		public int mainTextureID = -1;
+		public int colorID = -1;
+		public int sourceColorBlendModeID = -1;
+		public int destinationColorBlendModeID = -1;
+		public int sourceAlphaBlendModeID = -1;
+		public int destinationAlphaBlendModeID = -1;
+		public int cullModeID = -1;
+		public int zWriteModeID = -1;
+		public int zTestModeID = -1;
+		public int depthOffsetFactorID = -1;
+		public int depthOffsetUnitsID = -1;
+		public int blendOpColorID = -1;
+		public int blendOpAlphaID = -1;
+
+		// Descriptions for the OnGUI buttons
+		public const string MAIN_TEXTURE_DESCRIPTION = "Main Texture";
+		public const string COLOR_DESCRIPTION = "Color";
+		public const string SOURCE_COLOR_BLEND_MODE_DESCRIPTION = "Source BlendMode:";
+		public const string DESTINATION_COLOR_BLEND_MODE_DESCRIPTION = "Destination BlendMode:";
+		public const string SOURCE_ALPHA_BLEND_MODE_DESCRIPTION = "Source BlendMode:";
+		public const string DESTINATION_ALPHA_BLEND_MODE_DESCRIPTION = "Destination BlendMode:";
+		public const string CULL_MODE_DESCRIPTION = "Cull Mode: ";
+		public const string Z_WRITE_MODE_DESCRIPTION = "Z Write Mode: ";
+		public const string Z_TEST_MODE_DESCRIPTION = "Z Test Mode: ";
+		public const string DEPTH_OFFSET_FACTOR_DESCRIPTION = "Depth Offset Factor: ";
+		public const string DEPTH_OFFSET_UNITS_DESCRIPTION = "Depth Offset Units: ";
+		public const string BLEND_OP_COLOR_DESCRIPTION = "BlendOp: ";
+		public const string BLEND_OP_ALPHA_DESCRIPTION = "BlendOp: ";
+
+		public override string ToString()
+		{
+			System.Text.StringBuilder sb = new System.Text.StringBuilder ();
+			sb.AppendLine(MAIN_TEXTURE + ": " + mainTextureID);
+			sb.AppendLine(COLOR + ": " + colorID);
+			sb.AppendLine(SOURCE_COLOR_BLEND_MODE + ": " + sourceColorBlendModeID);
+			sb.AppendLine(DESTINATION_COLOR_BLEND_MODE + ": " + destinationColorBlendModeID);
+			sb.AppendLine(SOURCE_ALPHA_BLEND_MODE + ": " + sourceAlphaBlendModeID);
+			sb.AppendLine(DESTINATION_ALPHA_BLEND_MODE + ": " + destinationAlphaBlendModeID);
+			sb.AppendLine(CULL_MODE + ": " + cullModeID);
+			sb.AppendLine(Z_WRITE_MODE + ": " + zWriteModeID);
+			sb.AppendLine(Z_TEST_MODE + ": " + zTestModeID);
+			sb.AppendLine(DEPTH_OFFSET_FACTOR + ": " + depthOffsetFactorID);
+			sb.AppendLine(DEPTH_OFFSET_UNITS + ": " + depthOffsetUnitsID);
+			sb.AppendLine(BLEND_OP_COLOR + ": " + blendOpColorID);
+			sb.AppendLine(BLEND_OP_ALPHA + ": " + blendOpAlphaID);
+			return sb.ToString ();
+		}
+	}
+	ShaderProperties shaderProperties = new ShaderProperties();
+
+	enum ZWriteMode {
+		On = 0, 
+		Off = 1,
+	}
+
+	enum ZTestMode
+	{
+		Less = 0,
+		Greater = 1,
+		LEqual = 2,
+		GEqual = 3,
+		Equal = 4,
+		NotEqual = 5,
+		Always = 6
+	}
+
+	private BlendMode sourceColorBlendMode;
+	private BlendMode destinationColorBlendMode;
+	private BlendMode sourceAlphaBlendMode;
+	private BlendMode destinationAlphaBlendMode;
+	private CullMode cullMode;
+	private ZWriteMode zWriteMode;
+	private ZTestMode zTestMode;
+	private float depthOffsetFactor;
+	private float depthOffsetUnits;
+	private BlendOp blendOpColor;
+	private BlendOp blendOpAlpha;
+	private Color color;
+
+	#endregion
+
+	#region MonoBehaviour
+
+	public override void OnInspectorGUI()
+	{
+		if (!isVisible)
+		{
+			return;
+		}
+
+		// Make sure we have all that we need to continue...
+		Init();
+
+		// Get the material..
+		Material material = target as Material;
+
+		// Get the current state of the variables in the Material/Shader
+		GetMaterialProperties(material);
+
+		// Expose the properties in the editor
+		ExposeMaterialProperties();
+
+		// Update the Material/Shader if needed
+		UpdateMaterialProperties(material);
+	}
+
+	private void Init()
+	{
+		if (shaderProperties.sourceColorBlendModeID == -1)
+		{
+			shaderProperties.mainTextureID = Shader.PropertyToID(ShaderProperties.MAIN_TEXTURE);
+			shaderProperties.colorID = Shader.PropertyToID(ShaderProperties.COLOR);
+			shaderProperties.sourceColorBlendModeID = Shader.PropertyToID(ShaderProperties.SOURCE_COLOR_BLEND_MODE);
+			shaderProperties.destinationColorBlendModeID = Shader.PropertyToID(ShaderProperties.DESTINATION_COLOR_BLEND_MODE);
+			shaderProperties.sourceAlphaBlendModeID = Shader.PropertyToID(ShaderProperties.SOURCE_ALPHA_BLEND_MODE);
+			shaderProperties.destinationAlphaBlendModeID = Shader.PropertyToID(ShaderProperties.DESTINATION_ALPHA_BLEND_MODE);
+			shaderProperties.cullModeID = Shader.PropertyToID(ShaderProperties.CULL_MODE);
+			shaderProperties.zWriteModeID = Shader.PropertyToID(ShaderProperties.Z_WRITE_MODE);
+			shaderProperties.zTestModeID = Shader.PropertyToID(ShaderProperties.Z_TEST_MODE);
+			shaderProperties.depthOffsetFactorID = Shader.PropertyToID(ShaderProperties.DEPTH_OFFSET_FACTOR);
+			shaderProperties.depthOffsetUnitsID = Shader.PropertyToID(ShaderProperties.DEPTH_OFFSET_UNITS);
+			shaderProperties.blendOpColorID = Shader.PropertyToID(ShaderProperties.BLEND_OP_COLOR);
+			shaderProperties.blendOpAlphaID = Shader.PropertyToID(ShaderProperties.BLEND_OP_ALPHA);
+			//Debug.Log (shaderProperties.ToString ());
+		}
+	}
+
+	private void GetMaterialProperties(Material material)
+	{
+		color = material.GetColor (shaderProperties.colorID);
+		sourceColorBlendMode = (BlendMode) material.GetInt(shaderProperties.sourceColorBlendModeID);
+		destinationColorBlendMode = (BlendMode) material.GetInt(shaderProperties.destinationColorBlendModeID);
+		sourceAlphaBlendMode = (BlendMode) material.GetInt(shaderProperties.sourceAlphaBlendModeID);
+		destinationAlphaBlendMode = (BlendMode) material.GetInt(shaderProperties.destinationAlphaBlendModeID);
+		cullMode = (CullMode)material.GetInt(shaderProperties.cullModeID);
+		zWriteMode = (ZWriteMode)material.GetInt(shaderProperties.zWriteModeID);
+		zTestMode = (ZTestMode)material.GetInt(shaderProperties.zTestModeID);
+		depthOffsetFactor = material.GetFloat (shaderProperties.depthOffsetFactorID);
+		depthOffsetUnits = material.GetFloat (shaderProperties.depthOffsetUnitsID);
+		blendOpColor = (BlendOp)material.GetInt (shaderProperties.blendOpColorID);
+		blendOpAlpha = (BlendOp)material.GetInt (shaderProperties.blendOpAlphaID);
+	}
+
+	private void ExposeMaterialProperties()
+	{
+		// Create a style for the titles
+		GUIStyle guiStyle = new GUIStyle();
+		guiStyle.fontStyle = FontStyle.Bold;
+		guiStyle.normal.textColor = Color.white;
+
+		// Start the check if anything has changed...
+		EditorGUI.BeginChangeCheck();
+
+		// Show the properties
+		EditorGUILayout.LabelField ("Main texture and color", guiStyle);
+		//TexturePropertySingleLine(new GUIContent(ShaderProperties.MAIN_TEXTURE_DESCRIPTION), GetMaterialProperties(targets)[0]);
+		TextureProperty(GetMaterialProperties(targets)[0], ShaderProperties.MAIN_TEXTURE_DESCRIPTION);
+		color = EditorGUILayout.ColorField(ShaderProperties.COLOR_DESCRIPTION, color);
+		EditorGUILayout.Separator();
+
+		EditorGUILayout.LabelField ("Color Blending", guiStyle);
+		sourceColorBlendMode = (BlendMode) EditorGUILayout.EnumPopup(ShaderProperties.SOURCE_COLOR_BLEND_MODE_DESCRIPTION, sourceColorBlendMode);
+		destinationColorBlendMode = (BlendMode) EditorGUILayout.EnumPopup(ShaderProperties.DESTINATION_COLOR_BLEND_MODE_DESCRIPTION, destinationColorBlendMode);
+		blendOpColor = (BlendOp)EditorGUILayout.EnumPopup(ShaderProperties.BLEND_OP_COLOR_DESCRIPTION, blendOpColor);
+		EditorGUILayout.Separator();
+
+		EditorGUILayout.LabelField ("Alpha Blending", guiStyle);
+		sourceAlphaBlendMode = (BlendMode) EditorGUILayout.EnumPopup(ShaderProperties.SOURCE_ALPHA_BLEND_MODE_DESCRIPTION, sourceAlphaBlendMode);
+		destinationAlphaBlendMode = (BlendMode) EditorGUILayout.EnumPopup(ShaderProperties.DESTINATION_ALPHA_BLEND_MODE_DESCRIPTION, destinationAlphaBlendMode);
+		blendOpAlpha = (BlendOp)EditorGUILayout.EnumPopup(ShaderProperties.BLEND_OP_ALPHA_DESCRIPTION, blendOpAlpha);
+		EditorGUILayout.Separator();
+
+		EditorGUILayout.LabelField ("Culling", guiStyle);
+		cullMode = (CullMode)EditorGUILayout.EnumPopup(ShaderProperties.CULL_MODE_DESCRIPTION, cullMode);
+		EditorGUILayout.Separator();
+
+		EditorGUILayout.LabelField ("Z Write/Test/Depth", guiStyle);
+		zWriteMode = (ZWriteMode)EditorGUILayout.EnumPopup(ShaderProperties.Z_WRITE_MODE_DESCRIPTION, zWriteMode);
+		zTestMode = (ZTestMode)EditorGUILayout.EnumPopup(ShaderProperties.Z_TEST_MODE_DESCRIPTION, zTestMode);
+		depthOffsetFactor = EditorGUILayout.FloatField(ShaderProperties.DEPTH_OFFSET_FACTOR_DESCRIPTION, depthOffsetFactor);
+		depthOffsetUnits = EditorGUILayout.FloatField(ShaderProperties.DEPTH_OFFSET_UNITS_DESCRIPTION, depthOffsetUnits);
+	}
+
+	private void UpdateMaterialProperties(Material material)
+	{
+		// End the check if anything had changed
+		if (EditorGUI.EndChangeCheck())
+		{
+			material.SetColor(shaderProperties.colorID, color);
+			material.SetInt(shaderProperties.sourceColorBlendModeID, (int)sourceColorBlendMode);
+			material.SetInt(shaderProperties.destinationColorBlendModeID, (int)destinationColorBlendMode);
+			material.SetInt(shaderProperties.sourceAlphaBlendModeID, (int)sourceAlphaBlendMode);
+			material.SetInt(shaderProperties.destinationAlphaBlendModeID, (int)destinationAlphaBlendMode);
+			material.SetInt(shaderProperties.blendOpColorID, (int) blendOpColor);
+			material.SetInt(shaderProperties.blendOpAlphaID, (int) blendOpAlpha);
+			material.SetInt(shaderProperties.cullModeID, (int) cullMode);
+			material.SetInt(shaderProperties.zWriteModeID, (int) zWriteMode);
+			material.SetInt(shaderProperties.zTestModeID, (int) zTestMode);
+			material.SetFloat(shaderProperties.depthOffsetFactorID, depthOffsetFactor);
+			material.SetFloat(shaderProperties.depthOffsetUnitsID, depthOffsetUnits);
+
+			EditorUtility.SetDirty(material);
+		}
+	}
+
+	#endregion
+}
